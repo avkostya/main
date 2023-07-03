6913ca206ae39796b02793b3e98ba6f7a3ee65b1
avkostya <138392776+avkostya@users.noreply.github.com>
2023-07-03 20:55:22 +0300

    Add files via upload

---
+++ PythonApplication7.py
@@ -0,0 +1,12 @@
+﻿print("Введите  минимальную сумму инвестиции")
+min = int(input ())
+print("Введите  сумму доступную Майклу")
+maikl = int(input ())
+print("Введите  сумму доступную Ивану")
+ivan = int(input ())
+if (ivan>=min) and (maikl>=min):
+    print(2)
+elif (ivan>=min) or (maikl>=min):
+    print(1)
+else:
+    print(0)
